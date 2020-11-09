263dbdce796620d2a90b754e7a55098c09749b8b
杨嘉豪 <hchopper@163.com>
2020-11-09 21:17:00 +0800

    update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,8 +10,9 @@
         </div>
         <div id="navinfo" v-if="!islogin">
             <i class="el-icon-s-home" style="font-weight:bolder" @click="gohome"> 首页</i>
-            <div id="logindiv">
-                <div  style="font-weight:bolder" @click="loginadmin"> 立即登陆</div>
+            <div id="loginbtndiv">
+                <div  style="font-weight:bolder" @click="registeradmin"> 立即注册</div>
+                <div  style="font-weight:bolder;margin-left:20px" @click="loginadmin"> 立即登陆</div>
             </div>
         </div>
       </div>
@@ -29,6 +30,24 @@
             <el-button type="primary" @click="login">确 定</el-button>
         </span>
         </el-dialog>
+        <el-dialog
+        title="登陆"
+        :visible.sync="centerDialogVisible3"
+        width="50%"
+        center>
+        账号
+        <el-input v-model="reusername" placeholder="请输入内容"></el-input>
+        密码
+        <el-input v-model="repassword" placeholder="请输入密码" type="password"></el-input>
+        确认密码
+        <el-input v-model="arepassword" placeholder="请重新输入密码" type="password"></el-input>
+        姓名
+        <el-input v-model="rename" placeholder="输入姓名"></el-input>
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="centerDialogVisible3 = false">取 消</el-button>
+            <el-button type="primary" @click="register">确 定</el-button>
+        </span>
+        </el-dialog>
   </div>
 </template>
 
@@ -37,10 +56,15 @@ export default {
     data(){
         return{
             islogin:sessionStorage.isLogin,
+            reusername:'',
+            repassword:'',
+            arepassword:'',
+            rename:'',
             username:'',
             password:'',
             centerDialogVisible:false,
-            navheadimg:sessionStorage.getItem('headimg')
+            navheadimg:sessionStorage.getItem('headimg'),
+            centerDialogVisible3:false
         }
     },
     methods:{
@@ -113,14 +137,51 @@ export default {
         loginadmin(){
             this.centerDialogVisible = true
         },
+        registeradmin(){
+            this.centerDialogVisible3 = true
+        },
         gohome(){
             this.$router.push({name:"Home"})
+        },
+        register(){
+            if(this.reusername === '' || this.repassword === '' || this.arepassword === ''){
+                this.$message.error('请完成注册信息的填写');
+            }else if(this.repassword !== this.arepassword){
+                this.$message.error('两次输入的密码不一致');
+            }else{
+                var that= this
+            this.$axios({
+                url:'/register/adminRegister?username='+that.reusername+'&password='+that.repassword+'$realname'+that.rename,
+                method:'get'
+            })
+            .then(res=>{
+                console.log(res);
+                if(res.data.code==200){
+                    this.$message({
+                        showClose: true,
+                        message: res.data.msg,
+                        type: 'success'
+                        });
+                    this.centerDialogVisible3 = false
+                }
+                else if(res.data.code==201){
+                    this.$message.error(res.data.msg);
+                }
+                
+            })
+            .catch(error=>{
+                console.log(error);
+                console.log('请求失败')})   
+            }
         }
     }
 }
 </script>
 
 <style>
+    #loginbtndiv{
+        display: flex;
+    }
     .el-icon-s-home{
         cursor: pointer;
     }
@@ -143,7 +204,7 @@ export default {
         cursor: pointer;
     }
     #navinfo{
-        width: 200px;
+        width: 400px;
         font-size: 1.5em;
         display: flex;
         justify-content: space-between;
